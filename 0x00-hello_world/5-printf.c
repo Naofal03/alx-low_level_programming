05a8f8c80267f19f35cacc322e0ac7c1862e157a
AKANHO Naofal <Nflakanho@gmail.com>
2022-12-17 08:16:21 -0800

    My first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,10 @@
  * main - A program that print a line using the printf function
  * Return: 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
 		return (0);
 }
+
