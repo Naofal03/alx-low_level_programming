36fd6fcb4a0959811130999ddb9ae141d943da2a
AKANHO Naofal <Nflakanho@gmail.com>
2022-12-17 06:09:08 -0800

    My first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: # (Success)
+ */
+int main (void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
