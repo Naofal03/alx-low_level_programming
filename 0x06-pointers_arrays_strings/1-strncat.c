f4b7ffdb686ad101e5fa013a2b31dff37ca83789
AKANHO Naofal <Nflakanho@gmail.com>
2022-12-22 08:18:05 -0800

    My first commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
  * _strncat - concatenate two strings
  * using at most n bytes from src
  * @dest: input value
  * @src: input value
  * @n: input value
- *
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	i = 0;
-	while ( dest[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
@@ -26,5 +26,5 @@ char *_strncat(char *dest, char *src, int n)
 		j++;
 	}
 	dest[i] = '\0';
-	return(dest);
+	return (dest);
 }
